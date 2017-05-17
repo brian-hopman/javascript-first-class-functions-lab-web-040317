a16b4f03a0a5ee421b4f6edf2a21edc8b391da2a
brian-hopman <hopman_2@hotmail.com>
2017-05-16 20:48:09 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function countdown(callback) {
+  window.setTimeout(callback, 2000)
+}
+
+function callback() {
+
+}
+
+
+
+
+
+function createMultiplier(multiplierValue) {
+  return function (value) {
+    return multiplierValue * value
+  }
+}
+
+function multiplier(num1, num2) {
+  return num1 * num2
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
